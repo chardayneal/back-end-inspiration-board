57e733012f79615ab08112c0919920ef7c562a40
Christelle Nkera <scnkera@gmail.com>
2025-01-03 12:49:07 -0500

    Updated seed.py data

--- seed.py
+++ seed.py
@@ -3,15 +3,33 @@ from app.models.board import Board
 
 my_app = create_app()
 with my_app.app_context():
-    db.session.add(Board(title="Do laundry", owner="Alice"))
-    db.session.add(Board(title="Buy groceries", owner="Bob"))
-    db.session.add(Board(title="Prepare presentation", owner="Charlie"))
-    db.session.add(Board(title="Call the plumber", owner="David"))
-    db.session.add(Board(title="Workout", owner="Eve"))
-    db.session.add(Board(title="Finish book", owner="Frank"))
-    db.session.add(Board(title="Plan trip", owner="Grace"))
-    db.session.add(Board(title="Clean the car", owner="Hannah"))
-    db.session.add(Board(title="Pay utility bills", owner="Ivy"))
-    db.session.add(Board(title="Cook a special dinner", owner="Jack"))
-    db.session.add(Board(title="Organize the closet", owner="Kim"))
+
+    # seeds data into Board table
+    db.session.add(Board(title="Dream Big", owner="Alice"))
+    db.session.add(Board(title="Adventure Awaits", owner="Bob"))
+    db.session.add(Board(title="Chasing Goals", owner="Charlie"))
+    db.session.add(Board(title="Moments of Joy", owner="David"))
+    db.session.add(Board(title="Strength and Growth", owner="Eve"))
+    db.session.add(Board(title="Pages of Inspiration", owner="Frank"))
+    db.session.add(Board(title="Wanderlust Dreams", owner="Grace"))
+    db.session.add(Board(title="Sparkling Clean", owner="Hannah"))
+    db.session.add(Board(title="Balanced Living", owner="Ivy"))
+    db.session.add(Board(title="Flavors of Love", owner="Jack"))
+    db.session.add(Board(title="Harmony at Home", owner="Kim"))
+
+    # seeds data into Card table
+    db.session.add(Card(message="Dream Big", like_count=12))
+    db.session.add(Card(message="Adventure Awaits", like_count=45))
+    db.session.add(Card(message="Chasing Goals", like_count=30))
+    db.session.add(Card(message="Moments of Joy", like_count=67))
+    db.session.add(Card(message="Strength and Growth", like_count=23))
+    db.session.add(Card(message="Pages of Inspiration", like_count=56))
+    db.session.add(Card(message="Wanderlust Dreams", like_count=89))
+    db.session.add(Card(message="Sparkling Clean", like_count=34))
+    db.session.add(Card(message="Balanced Living", like_count=50))
+    db.session.add(Card(message="Flavors of Love", like_count=76))
+    db.session.add(Card(message="Harmony at Home", like_count=41))
+
+
+
     db.session.commit()
